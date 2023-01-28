6a60839e555e4b39f270f24ab57597f5ac6713e0
Shegda Igor <igorbeef@gmail.com>
2023-01-28 22:05:51 +0200

    add Login and Registr

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,3 +1,29 @@
-export const RegistrationForm = () => {
-  return <div>RegistrationForm</div>;
+import css from './RegistrationForm.module.css';
+
+const RegistrationForm = () => {
+  return (
+    <section>
+      <form className={css.loginForm}>
+        <h4 className={css.title}>Register</h4>
+        <label className={css.regName}>
+          Name*
+          <input type="text" />
+        </label>
+        <label className={css.regEmail}>
+          Email*
+          <input type="text" />
+        </label>
+        <label className={css.regPassword}>
+          Password*
+          <input type="text" />
+        </label>
+        <button type="submit" className={css.regButton}>
+          Register
+        </button>
+        <button type="button">Log In</button>
+      </form>
+    </section>
+  );
 };
+
+export default RegistrationForm;
