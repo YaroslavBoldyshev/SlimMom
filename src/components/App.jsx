7347878d17b4f7da905196f0912f59e18830a303
YaroslavBoldyshev <110547217+YaroslavBoldyshev@users.noreply.github.com>
2023-01-30 13:57:18 +0200

    qwerty

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import PublicRoute from './PublicRoute/PublicRoute';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import NotFound from '../pages/NotFound/NotFound';
+import Container from './Container/Container';
 
 const Home = lazy(() => import('pages/Home/Home'));
 const Login = lazy(() => import('pages/Login/Login'));
@@ -14,24 +15,23 @@ const Main = lazy(() => import('pages/Main/Main'));
 const Dairy = lazy(() => import('pages/Dairy/Dairy'));
 const Calculator = lazy(() => import('pages/Calculator/Calculator'));
 
-export const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/" element={<PrivateRoute />}>
-            <Route path="/diary" element={<Dairy />} />
-            <Route path="/calculator" element={<Calculator />} />
-          </Route>
-          <Route path="/" element={<PublicRoute />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Registration />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
+export const App = () => (
+  <><Container>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/diary" element={<Dairy />} />
+          <Route path="/calculator" element={<Calculator />} />
         </Route>
-      </Routes>
-    </>
-  );
-};
+        <Route path="/" element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+    </Container>
+  </>
+);
