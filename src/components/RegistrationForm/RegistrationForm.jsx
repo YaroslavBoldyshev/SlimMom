c2066f9c4edfa463f3eb01c87e32717615ffe3aa
Shegda Igor <igorbeef@gmail.com>
2023-01-30 20:22:54 +0200

    styled add

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,28 +1,95 @@
-import css from './RegistrationForm.module.css';
+import Container from 'components/Container/Container';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+// import css from './RegistrationForm.module.css';
+import { register, logIn, logOut, refresh } from './auth-operations';
+import {
+  Input,
+  Label,
+  LabelPass,
+  LogButton,
+  RegButton,
+  RegisterForm,
+  Title,
+} from './RegistrationForm.styled';
 
 const RegistrationForm = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        return setUsername(value);
+      case 'password':
+        return setPassword(value);
+      case 'email':
+        return setEmail(value);
+      default:
+        return;
+    }
+  };
+
+  const authOperations = { register, logIn, logOut, refresh };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formData = { username, email: email.toLowerCase(), password };
+    dispatch(authOperations.register(formData))
+      .unwrap()
+      .then(() => {
+        dispatch(logIn({ email: email.toLowerCase(), password }));
+      })
+      .catch(error => error(error.message));
+
+    setUsername('');
+    setEmail('');
+    setPassword('');
+  };
+
   return (
-    <section>
-      <form className={css.loginForm}>
-        <h4 className={css.title}>Register</h4>
-        <label className={css.regName}>
+    <Container>
+      {/* <section> */}
+      <RegisterForm onSubmit={handleSubmit}>
+        <Title>Register</Title>
+        <Label>
           Name*
-          <input type="text" />
-        </label>
-        <label className={css.regEmail}>
+          <Input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={username}
+          />
+        </Label>
+        <Label>
           Email*
-          <input type="text" />
-        </label>
-        <label className={css.regPassword}>
+          <Input
+            type="text"
+            name="email"
+            onChange={handleChange}
+            value={email}
+          />
+        </Label>
+        <LabelPass>
           Password*
-          <input type="text" />
-        </label>
-        <button type="submit" className={css.regButton}>
-          Register
-        </button>
-        <button type="button">Log In</button>
-      </form>
-    </section>
+          <Input
+            type="text"
+            name="password"
+            onChange={handleChange}
+            value={password}
+          />
+        </LabelPass>
+        <RegButton type="submit">Register</RegButton>
+        <NavLink to="/login">
+          <LogButton type="button">Log In</LogButton>
+        </NavLink>
+      </RegisterForm>
+      {/* </section> */}
+    </Container>
   );
 };
 
--- src/components/RegistrationForm/RegistrationForm.styled.js
+++ src/components/RegistrationForm/RegistrationForm.styled.js
@@ -1,40 +1,44 @@
-/* .title {
+import styled from 'styled-components';
+
+export const RegisterForm = styled.form`
+  width: 400px;
+`;
+
+export const Title = styled.h4`
   margin-bottom: 55px;
   color: #fc842d;
   text-transform: uppercase;
-}
+`;
 
-.loginForm {
-  width: 400px;
-}
-
-.loginForm label {
+export const Label = styled.label`
   display: flex;
   flex-direction: column;
   color: #9b9faa;
   font-weight: 700;
   font-size: 14px;
   letter-spacing: 0.04em;
-}
+`;
 
-.loginForm input {
+export const LabelPass = styled.label`
+  margin-bottom: 60px;
+`;
+
+export const Input = styled.input`
   width: 240px;
   border: none;
   font-size: 16px;
   border-bottom: 1px solid #e0e0e0;
   outline: none;
-}
+`;
 
-.regEmail,
-.regName {
-  margin-bottom: 40px;
-}
+export const RegButton = styled.button`
+  color: white;
+  background-color: #fc842d;
+  margin-right: 32px;
+  box-shadow: 0px 4px 10px rgba(252, 132, 45, 0.5);
+`;
 
-.regPassword {
-  margin-bottom: 60px;
-}
-
-.loginForm button {
+export const LogButton = styled.button`
   width: 182px;
   padding: 13px 15px;
   font-size: 14px;
@@ -45,11 +49,4 @@
   border-radius: 30px;
   border: 2px solid #fc842d;
   box-shadow: 0px 4px 10px rgba(252, 132, 45, 0.5);
-}
-
-button.regButton {
-  color: white;
-  background-color: #fc842d;
-  margin-right: 32px;
-  box-shadow: 0px 4px 10px rgba(252, 132, 45, 0.5);
-} */
+`;
