cfb4580217672f0a667f98a6de5d5d81cde66dcb
Alex-underr <4ubs.pl@gmail.com>
2023-02-01 16:02:40 +0200

    Update RightSideBar.jsx

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -4,33 +4,33 @@ export const RightSideBar = () => {
   return (
     <Bar>
       <div>
-        <H2>Summary for 20/06/2020</H2>
+        <H2>Summary for RENDER</H2>
         <Ul>
           <List>
             <span>Left</span>
-            <span>Render</span>
+            <span>RENDER</span>
           </List>
           <List>
             <span>Consumed</span>
-            <span>Render</span>
+            <span>RENDER</span>
           </List>
           <List>
             <span>rate</span>
-            <span>Render</span>
+            <span>RENDER</span>
           </List>
           <List>
             <span>n% normal</span>
-            <span>Render</span>
+            <span>RENDER</span>
           </List>
         </Ul>
       </div>
       <div>
-        <H2 className="headerBottom"> Food not recommended</H2>
+        <H2 className="headerBottom">Food not recommended</H2>
         <Ul>
-          <List>Render</List>
-          <List>Render</List>
-          <List>Render</List>
-          <List>Render</List>
+          <List>RENDER</List>
+          <List>RENDER</List>
+          <List>RENDER</List>
+          <List>RENDER</List>
         </Ul>
       </div>
     </Bar>
