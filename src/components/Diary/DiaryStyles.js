24a7b94f99649d7c6e9b3558b0aaebec6a0287dd
Alex-underr <4ubs.pl@gmail.com>
2023-02-01 15:54:53 +0200

    RightSideBar
    Component, Style,

---
+++ src/components/Diary/DiaryStyles.js
@@ -0,0 +1,11 @@
+// import styled from '@emotion/styled';
+// export const Div = styled.div`
+//   display: flex;
+//   flex-direction: column;
+
+//   @media screen and (min-width: 768px) {
+//   }
+//   @media screen and (min-width: 1280px) {
+//     flex-direction: row;
+//   }
+// `;
--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,3 +1,38 @@
+import { Bar, Ul, H2, List } from './RightSideBarStyle';
+
 export const RightSideBar = () => {
-  return <div>RightSideBar</div>;
+  return (
+    <Bar>
+      <div>
+        <H2>Summary for 20/06/2020</H2>
+        <Ul>
+          <List>
+            <span>Left</span>
+            <span>Render</span>
+          </List>
+          <List>
+            <span>Consumed</span>
+            <span>Render</span>
+          </List>
+          <List>
+            <span>rate</span>
+            <span>Render</span>
+          </List>
+          <List>
+            <span>n% normal</span>
+            <span>Render</span>
+          </List>
+        </Ul>
+      </div>
+      <div>
+        <H2 className="headerBottom"> Food not recommended</H2>
+        <Ul>
+          <List>Render</List>
+          <List>Render</List>
+          <List>Render</List>
+          <List>Render</List>
+        </Ul>
+      </div>
+    </Bar>
+  );
 };
