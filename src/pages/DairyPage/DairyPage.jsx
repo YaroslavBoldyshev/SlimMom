974acbdcdfddcc685cb14ddfa1e05573ecad8b3b
YaroslavBoldyshev <110547217+YaroslavBoldyshev@users.noreply.github.com>
2023-02-02 16:33:22 +0200

    Update DairyPage.jsx

--- src/pages/DairyPage/DairyPage.jsx
+++ src/pages/DairyPage/DairyPage.jsx
@@ -1,11 +1,9 @@
 import { Diary } from 'components/Diary/Diary';
-import { RightSideBar } from 'components/RightSideBar/RightSideBar';
 
 export default function DairyPage() {
   return (
     <div>
       <Diary />
-      <RightSideBar />;
     </div>
   );
 }
