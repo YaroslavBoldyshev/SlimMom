4a5819434bffcb1c8642a14892df1e6f74341343
YaroslavBoldyshev <110547217+YaroslavBoldyshev@users.noreply.github.com>
2023-02-02 16:23:21 +0200

    right side bar moved to page Diary

--- src/components/Diary/Diary.jsx
+++ src/components/Diary/Diary.jsx
@@ -1,13 +1,9 @@
 import { DiaryAddProductForm } from 'components/DiaryAddProductForm/DiaryAddProductForm';
-import { RightSideBar } from 'components/RightSideBar/RightSideBar';
-
-
 
 export const Diary = () => {
   return (
     <div>
       <DiaryAddProductForm />
-      <RightSideBar />;
     </div>
   );
 };
